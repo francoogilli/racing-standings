f1c7035ceae2a24d059a0cbaa2913e42ecd61899
Franco Gilli <francogilli10@gmail.com>
2025-01-16 19:47:53 -0300

    Cambios

--- src/app/motogp/standings/page.tsx
+++ src/app/motogp/standings/page.tsx
@@ -5,12 +5,10 @@ import { WorldStanding } from "@/api/types";
 import { useEffect, useState } from "react";
 
 export default function MotoGpStandings() {
-  const [standings, setStandings] = useState<WorldStanding[]>([]);
 
   const fetchSeasonsData = async () => {
     try {
       const seasonsData = await fetchStandings();
-      setStandings([seasonsData]);
     } catch (error) {
       console.error("Error fetching standings:", error);
     }
