b99211a187bcdbd1c87bef2b4bcee631e693941b
Franco Gilli <francogilli10@gmail.com>
2025-01-17 19:39:24 -0300

    Quitar borde

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -3,7 +3,7 @@ export const Button = ({ text, href }: { text: string; href: string }) => {
     <a
       href={href}
       rel="noopener noreferrer"
-      className="px-5 py-3 font-clash font-medium border flex justify-center w-full md:w-auto gap-2 transition items-center effect01 bg-black text-white "
+      className="px-5 py-3 font-clash font-medium flex justify-center w-full md:w-auto gap-2 transition items-center effect01 bg-black text-white "
     >
       <slot name="icon-left" />
       <span>{text}</span>
