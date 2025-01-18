7cd6bab96282de859dbaee66e09683127fcc169b
Franco Gilli <francogilli10@gmail.com>
2025-01-18 11:09:38 -0300

    Actualizar estilos del componente Hero y modificar el ícono Trophy para mejorar la presentación

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -152,12 +152,12 @@ const Hero = () => {
           <div style={{ opacity: 1, transform: "none" }}>
             <div className="flex items-center gap-2 mt-4 ">
               <a
-                className="inline-flex items-center justify-center gap-2 whitespace-nowrap text-base font-medium ring-offset-background  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 bg-[#0091ff]  shadow-innerGlow text-primary-foreground hover:bg-[#0091ff]/90 transition-all duration-300 hover:ring-2 hover:ring-primary hover:ring-offset-2 h-12 rounded-md px-4 group overflow-hidden"
-                href="#projects"
+                className="inline-flex text-black items-center justify-center gap-2 whitespace-nowrap text-base font-semibold ring-offset-background  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 bg-gradient-to-tr from-[#ffde65] to-[#fdb600]  shadow-innerGlow text-primary-foreground transition-all duration-300 hover:ring-1 hover:ring-[#ffde65] hover:ring-offset-2 h-12 rounded-md px-4 group overflow-hidden"
+                href="#"
               >
                 <div className="font-clash translate-y-0 overflow-hidden font-medium flex flex-row gap-2 items-center opacity-100 transition group-hover:-translate-y-[150%] group-hover:opacity-0">
                   <Trophy />
-                  ¡Mira el Campeonato!
+                  Ver campeonato
                 </div>
                 <div className="absolute translate-y-[150%] opacity-0 transition group-hover:translate-y-0 group-hover:opacity-100">
                   <svg
@@ -167,7 +167,7 @@ const Hero = () => {
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    strokeWidth="2"
+                    strokeWidth="2.4"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     className="lucide lucide-arrow-down"
@@ -180,7 +180,7 @@ const Hero = () => {
               <a
                 target="_blank"
                 rel="noreferrer"
-                className="font-clash inline-flex items-center justify-center gap-2 whitespace-nowrap text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 dark:bg-neutral-900 bg-white hover:bg-gray-200 dark:hover:bg-neutral-800 text-secondary-foreground h-12 rounded-md px-6 dark:!bg-neutral-800/70 backdrop-blur"
+                className="font-clash inline-flex items-center justify-center gap-2 whitespace-nowrap text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 bg-neutral-900 hover:bg-neutral-800 text-secondary-foreground h-12 rounded-md px-6  backdrop-blur"
                 href="https://cdn.zegieweb.com/zegie-dev-images/cv.pdf"
               >
                 <Calendar />
