91ab1d120c2201b14d3ae01cff044e7f0f7283b1
Franco Gilli <francogilli10@gmail.com>
2025-01-22 18:42:44 -0300

    Corregir propiedades de estilo en el componente Calendar para usar notación camelCase

--- src/app/icons/Calendar.tsx
+++ src/app/icons/Calendar.tsx
@@ -11,9 +11,9 @@ export default function Calendar({ className }: Props) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={className}    >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" />
