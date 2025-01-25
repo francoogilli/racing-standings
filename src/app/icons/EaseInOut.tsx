dc58cbfa0dcafaa7e9d47d9b7a83b90c4f15890c
Franco Gilli <francogilli10@gmail.com>
2025-01-25 15:23:09 -0300

    Agregar soporte para personalizar el ancho del trazo en el componente EaseInOut

--- src/app/icons/EaseInOut.tsx
+++ src/app/icons/EaseInOut.tsx
@@ -1,7 +1,8 @@
 interface Props {
     className?: string
+    strokeWidth?: number
 }
-export default function EaseInOut({ className }: Props) {
+export default function EaseInOut({ className, strokeWidth = 2 }: Props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -10,7 +11,7 @@ export default function EaseInOut({ className }: Props) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth={strokeWidth}
       strokeLinecap="round"
       strokeLinejoin="round"
       className={className}    >
